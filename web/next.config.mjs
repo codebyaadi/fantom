ff945a42aa9a7c52697b7d69f22455720f745628
codebyaadi <adityastar57@gmail.com>
2024-07-07 18:26:03 +0530

    chore: setup for .env

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,3 +1,10 @@
+import { fileURLToPath } from 'url';
+import createJITI from 'jiti';
+const jiti = createJITI(fileURLToPath(import.meta.url));
+
+jiti("./src/env/client.ts");
+jiti("./src/env/server.ts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
---
+++ web/src/app/api/[[...route]]/products.ts
@@ -0,0 +1,9 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+    return c.json({ products: ["1", "2"]})
+})
+
+export default app;
