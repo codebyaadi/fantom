6331284263ab46d12f829d3e8c85430a7d65c0e0
codebyaadi <adityastar57@gmail.com>
2024-10-05 20:39:28 +0530

    config: site detail config added and metadata set

---
+++ src/config/site.ts
@@ -0,0 +1,16 @@
+export type SiteConfig = typeof siteConfig;
+
+const links = {
+  x: 'https://twitter.com/codebyaadi',
+  github: 'https://github.com/codebyaadi',
+  githubAccount: 'https://github.com/codebyaadi',
+};
+
+export const siteConfig = {
+  name: 'Fantom.',
+  description:
+    'Decentralized platform to read you beloved manga, manhua and manhwa - also to buy exclusive artwork and character designs as NFTs.',
+  url: 'https://',
+  ogImage: 'https://',
+  links,
+};
