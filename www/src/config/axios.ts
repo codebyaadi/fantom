fd883c8789680ca87660478378714b6b006ee964
codebyaadi <adityastar57@gmail.com>
2024-10-05 18:59:56 +0530

    chore: www removed

--- www/src/config/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios, { AxiosInstance } from "axios";
-import { env } from "@/env";
-
-const axiosInstance: AxiosInstance = axios.create({
-  baseURL: env.NEXT_PUBLIC_HONO_API_URL,
-  headers: { "Content-Type": "application/json" },
-});
-
-export { axiosInstance };
--- www/src/lib/queries/product-queries.ts
+++
@@ -1,7 +0,0 @@
-import { axiosInstance } from "@/config/axios";
-import { Product } from "@/types";
-
-export async function getAllProducts() {
-    const res = await axiosInstance.get("/fetch-all-products");
-    return res.data as Product[];
-};
